7f3c52412c587e58fd7b6e928d8efb04824d04a5
sdyushembieva <dyushembieva@gmail.com>
2018-02-27 19:39:27 +0600

    First commit

---
+++ test1.py
@@ -0,0 +1,13 @@
+num = range(1, 100)
+
+for x in num:
+    if x % 3 == 0 and x % 5 == 0:
+        print('LoloPepe')
+    elif x % 3 == 0:
+        print('Lolo')
+    elif x % 5 == 0:
+        print('Pepe')
+    else:
+        print(x)
+
+
